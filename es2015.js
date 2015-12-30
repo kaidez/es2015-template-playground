85faba9272301da4ec7c5c430bbee44ab94c4985
Kai Gittens <kai.gittens@gmail.com>
2015-12-30 09:19:29 -0500

    add comments & whitespace

--- es2015.js
+++ es2015.js
@@ -1,5 +1,11 @@
 function PrintTeamES2015( team ) {
-  document.getElementById("containerTwo").innerHTML = `${team.city} ${team.teamName}`;
+
+  /*
+   * Templates are defined bu the "`" charater, which is (probably) to
+   * the left of the "1" button on your keyboard
+   */
+  document.getElementById( "containerTwo" ).innerHTML = `${team.city} ${team.teamName}`;
+
 }
 
-var kai = new PrintTeamES2015({city:"Brooklyn", teamName:"Nets" });
+var kai = new PrintTeamES2015( {city:"Brooklyn", teamName:"Nets" } );
